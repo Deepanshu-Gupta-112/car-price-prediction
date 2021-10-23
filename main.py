2432d9c5fa3c878fdb32801c0b55f8feec531760
deepanshu <deepanshu19csu078@ncuindia.edu>
2021-10-23 20:24:12 +0530

    Changes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def Home():
 
 
 standard_to = StandardScaler()
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=['POST', 'GET'])
 def predict():
     Fuel_Type_Diesel=0
     if request.method == 'POST':
@@ -23,6 +23,7 @@ def predict():
         Kms_Driven2=np.log(Kms_Driven)
         Owner=int(request.form['Owner'])
         Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+
         if(Fuel_Type_Petrol=='Petrol'):
                 Fuel_Type_Petrol=1
                 Fuel_Type_Diesel=0
@@ -43,7 +44,7 @@ def predict():
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+            return render_template('index.html',prediction_text="Sorry you cannot sell this car")
         else:
             return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
     else:
